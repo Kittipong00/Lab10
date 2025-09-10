cfb6d651a14efa94dc8323a7075891d87b9e5bbd
Kittipong00 <kittipong.mu@ku.th>
2025-09-10 21:38:35 +0700

    Update TestRunner.java

--- TestRunner.java
+++ TestRunner.java
@@ -36,8 +36,27 @@ public class TestRunner {
 
         System.out.println("\n--- 3. Testing Factory and Decorator Pattern (Shipment) ---");
         //สร้างการจัดส่งแบบมาตรฐาน
-        Shipment.standardShipment = shipmentFactory.createShipment("STANDARD");
+        Shipment standardShipment = shipmentFactory.createShipment("STANDARD");
+        System.out.println("Base Shipment: " + standardShipment.getInfo() + ", Cost: " + standardShipment.getCost());
 
+        // "ห่อ" ด้วยบริการห่อของขวัญ
+        Shipment giftWrapped = new GiftWrapDecorator(standardShipment);
+        System.out.println("Deprecated: " + giftWrapped.getInfo() + ", Cost: " + giftWrapped.getCost());
+
+        // "ห่อ" ทับด้วยบริการประกันสินค้า
+        Shipment fullyLoaded  = new InsuranceDecorator(giftWrapped, myOrder);
+        System.out.println("Fully Deprecated: " + fullyLoaded.getInfo() + ", Cost: " + fullyLoaded.getCost());
+
+        System.out.println("\n--- 4. Printing Final Summary ---");
+        double finalPrice = priceAfterPercentage; // สมมติว่าใช้ส่วนลด 10%
+        double totalCost = finalPrice + fullyLoaded.getCost();
+        System.out.println("Final price after discount: " + finalPrice);
+        System.out.println("Final shipment cost: " + fullyLoaded.getCost());
+        System.out.println("TOTAL TO PAY: " + totalCost);
+        
+        // --- 5. Testing Observer Pattern (Processing Order) ---
+        orderProcessor.processOrder(myOrder);
 
     }
 }
+
